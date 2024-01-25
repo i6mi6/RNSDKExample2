17525de412aaa0af68f173c1a8fc32278cdc96b2
i6mi6 <azlibe1995@gmail.com>
2024-01-25 18:17:05 +0200

    remove unused code

--- src/InnerContainer.js
+++ src/InnerContainer.js
@@ -3,20 +3,12 @@ import React from 'react'
 import {
   NativeEventEmitter,
   NativeModules,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
 } from 'react-native'
 import { compose } from 'recompose'
 import { EVENT_TYPES, VIEW_TYPE } from './constants'
 import { withStorelinkConsumer } from './utils/hoc'
-import { logEventDev, logEventDebug, logError } from './utils/util'
+import { logError, logEventDebug, logEventDev } from './utils/util'
 const { StorelinkModule } = NativeModules
-
-// Walmart
-const STORE_ID = "U3RvcmVOb2RlOjM="
-const STORE_NAME = "Walmart"
 
 class InnerContainer extends React.Component {
 
@@ -28,9 +20,6 @@ class InnerContainer extends React.Component {
     if (viewType === VIEW_TYPE.TRANSFER_CART) {
       return <TransferCartContainerView {...this.props} />
     }
-    // if (viewType === VIEW_TYPE.STORE_CONNECTIONS_LIST) {
-    //   return <StoreConnectionsListContainerView {...this.props} />
-    // }
     if (viewType === VIEW_TYPE.BACKGROUND_TASK) {
       return <BackgroundContainerView {...this.props} />
     }
@@ -139,35 +128,6 @@ const TransferCartInnerContainer = compose(
   withStorelinkConsumer,
 )(TransferCartInnerContainerPure)
 
-
-class StoreConnectionsListContainerViewPure extends React.Component {
-
-  renderContent = () => {
-    return (
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => this.props.connectOrUpdateStoreConnection({ storeId: STORE_ID })}>
-        <Text>{`Connect`} {STORE_NAME}</Text>
-      </TouchableOpacity>
-    )
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>CooklistSDK Wrapper</Text>
-          {this.renderContent()}
-        </View>
-      </View>
-    )
-  }
-}
-
-const StoreConnectionsListContainerView = compose(
-  withStorelinkConsumer,
-)(StoreConnectionsListContainerViewPure)
-
 class BackgroundContainerViewPure extends React.Component {
 
   eventEmitter = null
@@ -209,12 +169,3 @@ const BackgroundContainerView = compose(
 )(BackgroundContainerViewPure)
 
 export default InnerContainer
-
-const styles = StyleSheet.create({
-  button: {
-    padding: 5,
-    borderWidth: 1,
-    borderRadius: 6,
-    margin: 10,
-  },
-})
