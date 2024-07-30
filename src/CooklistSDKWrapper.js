864ca5d79444bdc5c52598cf752d0802d0f65fe9
i6mi6 <azlibe1995@gmail.com>
2024-07-31 00:32:58 +0300

    undo wrapper

--- src/CooklistSDKWrapper.js
+++ src/CooklistSDKWrapper.js
@@ -20,30 +20,28 @@ class CooklistSDKWrapper extends React.Component {
     try {
       const { viewType, logLevel, refreshToken, brandName, logoUrl, _devApiLocation } = this.props
 
-      StorelinkModule.sampleMethod('test', console.log)
-
       const [sdkResponse] = await Promise.all([
-        // Storelink.configure({
-        //   refreshToken,
-        //   onStoreConnectionEvent: this.onStoreConnectionEvent,
-        //   onInvoiceEvent: this.onInvoiceEvent,
-        //   onCheckingStoreConnectionEvent: this.onCheckingStoreConnectionEvent,
-        //   _backgroundDisabled: viewType !== VIEW_TYPE.BACKGROUND_TASK,
-        //   _devApiLocation: _devApiLocation,
-        //   _logLevel: logLevel,
-        //   brandName,
-        //   logoUrl,
-        // }),
         Storelink.configure({
-          refreshToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo2ODMxMjEsIm9pZCI6MTAwOSwidmVyc2lvbiI6MSwianRpIjoiYjFjYjgzMWItMmY0MS00YTg1LThhNTUtMDk5M2U4MGY3OTBiIiwiaXNzdWVkX2F0IjoxNzIyMzY5NTQxLjk3MzE5NSwiZXhwaXJlc19hdCI6MTc1MzkwNTU0MS45NzMxOTUsInRva2VuX3R5cGUiOiJyZWZyZXNoIn0.BsjOG1ZwtuHx51xntqnnGrt5KWZ2_WKHrio0QeeuqZM',
+          refreshToken,
           onStoreConnectionEvent: this.onStoreConnectionEvent,
           onInvoiceEvent: this.onInvoiceEvent,
           onCheckingStoreConnectionEvent: this.onCheckingStoreConnectionEvent,
-          _devApiLocation: 'http://192.168.0.10:8000/gql',
-          _logLevel: LOG_LEVEL.DEV,
-          brandName: 'Cooklist',
-          logoUrl: 'https://play-lh.googleusercontent.com/1MgS_1nBA858MqMzhu-cqeXpbkTC3tVrshkj79IAuKhDlN7LZXdH4ECw6wiwA86vUQ',
+          _backgroundDisabled: viewType !== VIEW_TYPE.BACKGROUND_TASK,
+          _devApiLocation: _devApiLocation,
+          _logLevel: logLevel,
+          brandName,
+          logoUrl,
         }),
+        // Storelink.configure({
+        //   refreshToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo2ODMxMjEsIm9pZCI6MTAwOSwidmVyc2lvbiI6MSwianRpIjoiYjFjYjgzMWItMmY0MS00YTg1LThhNTUtMDk5M2U4MGY3OTBiIiwiaXNzdWVkX2F0IjoxNzIyMzY5NTQxLjk3MzE5NSwiZXhwaXJlc19hdCI6MTc1MzkwNTU0MS45NzMxOTUsInRva2VuX3R5cGUiOiJyZWZyZXNoIn0.BsjOG1ZwtuHx51xntqnnGrt5KWZ2_WKHrio0QeeuqZM',
+        //   onStoreConnectionEvent: this.onStoreConnectionEvent,
+        //   onInvoiceEvent: this.onInvoiceEvent,
+        //   onCheckingStoreConnectionEvent: this.onCheckingStoreConnectionEvent,
+        //   _devApiLocation: 'http://192.168.0.10:8000/gql',
+        //   _logLevel: LOG_LEVEL.DEV,
+        //   brandName: 'Cooklist',
+        //   logoUrl: 'https://play-lh.googleusercontent.com/1MgS_1nBA858MqMzhu-cqeXpbkTC3tVrshkj79IAuKhDlN7LZXdH4ECw6wiwA86vUQ',
+        // }),
       ])
       if (sdkResponse.success) {
         this.setState({ loading: false })
