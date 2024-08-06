e3e3302ff9c242275a9327429558006827fdd7bc
Bruno Villanova <bruno@digimundi.com.br>
2024-08-06 01:16:25 -0300

    chore: update example debug to use project and release to use aar libs dir

--- android/storelink/src/main/java/com/storelink/ReactNativeViewManager.java
+++ android/storelink/src/main/java/com/storelink/ReactNativeViewManager.java
@@ -2,11 +2,9 @@ package com.storelink;
 
 import android.app.Activity;
 import android.app.Application;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
-import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactPackage;
@@ -14,8 +12,6 @@ import com.facebook.react.ReactRootView;
 import com.facebook.react.common.LifecycleState;
 import com.facebook.soloader.SoLoader;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
-
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.List;
